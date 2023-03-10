1a8a380b7de3cd06566bf5cca1a0ed9c696f257d
Narien Kondam <nkondam@nariens-mbp.mynetworksettings.com>
2023-03-10 09:13:32 -0500

    first commit

---
+++ .github/scripts/test_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+
+excel_data = pd.read_excel('input.xlsx')
+
+# Convert Excel data to JSON
+json_data = excel_data.to_json(orient='records')
+
+# Set headers and payload for POST request
+headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {API_KEY}'}
+payload = json_data
+
+# Send POST request
+response = requests.post(API_URL, headers=headers, data=payload)
+
+# Print response
+print(response.text)
